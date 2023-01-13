3fa3026319a215d4e6b7bbf917df9f4ea0486fe0
Lumady <elodie.wsc@gmail.com>
2023-01-13 10:56:02 +0100

    ébauche 2

--- index.js
+++ index.js
@@ -6,3 +6,31 @@ const nemean = new Fighter("🦁 Nemean Lion", 11, 13);
 
 // use your Figher class here
 //console.log(heracles, nemean);
+
+const MAX_LIFE = 100;
+
+class Fighter {
+  constructor(name, strenght, dexterity) {
+    this.name = name;
+    this.strenght = strenght;
+    this.dexterity = dexterity;
+    this.life = MAX_LIFE;
+  }
+
+  fight(Defender) {
+    const impact = getRandomInt(this.dexterity);
+    const parade = getRandomInt(Defender.strenght);
+    const total = impact - parade < 0 ? 0 : total;
+    const newLife = Defender.life - total <= 0 ? 0 : newLife;
+    Defender.life = newLife;
+
+    return {};
+  }
+
+  getRandomInt(max) {
+    return 1 + Math.floor(Math.random() * max);
+  }
+  /*
+   **nouveaux points de vies du défenseur = points de vie actuels du défenseur - (dommages de l'attaquant - dextérité du défenseur)**
+   */
+}
