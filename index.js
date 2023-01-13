eb2a4bc243a803adc579ecf96a86136cbc54dcb1
Lumady <elodie.wsc@gmail.com>
2023-01-13 10:21:21 +0100

    ébauche de poo1

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 // First Labour : Heracles vs Nemean Lion
+const Fighter = require("./src/Fighter");
+
+const heracles = new Fighter("🧔 Heracles", 20, 6);
+const nemean = new Fighter("🦁 Nemean Lion", 11, 13);
+
 // use your Figher class here
+//console.log(heracles, nemean);
--- src/Fighter.js
+++ src/Fighter.js
@@ -1,1 +1,30 @@
 /* Fighter class definition */
+
+const MAX_LIFE = 100;
+
+class Fighter {
+  constructor(name, strenght, dexterity) {
+    this.name = name;
+    this.strenght = strenght;
+    this.dexterity = dexterity;
+    this.life = MAX_LIFE;
+  }
+
+  fight(Defender) {
+    const impact = getRandomInt(this.dexterity);
+    const parade = getRandomInt(Defender.strenght) < 0 ? 0 : parade;
+    const newLife = Defender.life - (impact - parade) <= 0 ? 0 : newLife;
+    Defender.life = newLife;
+
+    return {};
+  }
+
+  getRandomInt(max) {
+    return 1 + Math.floor(Math.random() * max);
+  }
+  /*
+   **nouveaux points de vies du défenseur = points de vie actuels du défenseur - (dommages de l'attaquant - dextérité du défenseur)**
+   */
+}
+
+module.exports = Fighter;
