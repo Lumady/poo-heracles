3d97b373126ec0c6709c67d56c17dbdbabb67905
Lumady <elodie.wsc@gmail.com>
2023-01-13 13:47:28 +0100

    marche

--- index.js
+++ index.js
@@ -4,33 +4,17 @@ const Fighter = require("./src/Fighter");
 const heracles = new Fighter("🧔 Heracles", 20, 6);
 const nemean = new Fighter("🦁 Nemean Lion", 11, 13);
 
-// use your Figher class here
-//console.log(heracles, nemean);
-
-const MAX_LIFE = 100;
-
-class Fighter {
-  constructor(name, strenght, dexterity) {
-    this.name = name;
-    this.strenght = strenght;
-    this.dexterity = dexterity;
-    this.life = MAX_LIFE;
-  }
-
-  fight(Defender) {
-    const impact = getRandomInt(this.dexterity);
-    const parade = getRandomInt(Defender.strenght);
-    const total = impact - parade < 0 ? 0 : total;
-    const newLife = Defender.life - total <= 0 ? 0 : newLife;
-    Defender.life = newLife;
-
-    return {};
-  }
-
-  getRandomInt(max) {
-    return 1 + Math.floor(Math.random() * max);
-  }
-  /*
-   **nouveaux points de vies du défenseur = points de vie actuels du défenseur - (dommages de l'attaquant - dextérité du défenseur)**
-   */
+while (heracles.life > 0 && nemean.life > 0) {
+  //console.log("denis");
+  heracles.fight(nemean);
+  nemean.fight(heracles);
 }
+
+// while (this.life > 0 && Defender.life > 0) {
+//   console.log("denis");
+//   this.fight(Defender);
+//   Defender.fight(this);
+// }
+
+// console.log(heracles);
+// console.log(nemean);
--- src/Fighter.js
+++ src/Fighter.js
@@ -3,25 +3,29 @@
 const MAX_LIFE = 100;
 
 class Fighter {
-  constructor(name, strenght, dexterity) {
+  constructor(name, strenght, parade) {
     this.name = name;
     this.strenght = strenght;
-    this.dexterity = dexterity;
+    this.parade = parade;
     this.life = MAX_LIFE;
   }
 
   fight(Defender) {
-    const impact = getRandomInt(this.dexterity);
-    const parade = getRandomInt(Defender.strenght) < 0 ? 0 : parade;
-    const newLife = Defender.life - (impact - parade) <= 0 ? 0 : newLife;
-    Defender.life = newLife;
+    const impact = 1 + Math.floor(Math.random() * this.parade);
+    const parade = 1 + Math.floor(Math.random() * Defender.strenght);
+    let total = impact - parade;
+    total = total < 0 ? 0 : total;
+    let newLife = Defender.life - total;
+    newLife = newLife <= 0 ? 0 : newLife;
+    Defender.life = Math.max(newLife, 0);
 
-    return {};
+    return console.log(`l'attaquant ${this.name} attaque ${Defender.name} ! \r\n 
+      il reste ${Defender.life} points de vie à ${Defender.name}`);
   }
 
-  getRandomInt(max) {
-    return 1 + Math.floor(Math.random() * max);
-  }
+  // getRandomInt(max) {
+  //   return 1 + Math.floor(Math.random() * max);
+  // }
   /*
    **nouveaux points de vies du défenseur = points de vie actuels du défenseur - (dommages de l'attaquant - dextérité du défenseur)**
    */
